24f63ed91d4ba7792d3d440e451daa5c24c01ecc
Flavio Gonzaga <flavio.gonzaga@intl.att.com>
2021-01-12 16:48:37 -0500

    Part 1

--- terraform/environment/dev-01/config.tf
+++ terraform/environment/dev-01/config.tf
@@ -1,30 +1,21 @@
 provider "aws" {
   region = "${var.region}"
-  profile = "dev-flavio"
+  profile = "dev-01"
 }
-
-//terraform {
-//  backend "s3" {
-//    bucket = "flavio-dev-test"
-//    key    = "states/demo.tfstate"
-//    region = "us-east-2"
-//    profile = "dev-flavio"
-//  }
-//}
 
 // Main configuration
 variable "region"               {default = "us-east-2"}
 variable "jar_file"             {default = "demo-aws-stepfunction-1.0.0-SNAPSHOT.jar"}
-variable "environment_prefix"   {default = "dev01"}
+variable "environment_prefix"   {default = "dev-01"}
 
 // AWS pre-existing resources
-variable "lambda_role_arn"      {default = "arn:aws:iam::473543219091:role/lambda-role-01"}
+variable "lambda_role_arn"      {default = "arn:aws:iam::473543219091:role/lambda-role"}
 
 module "core" {
   source                    = "../../module/core"
   region                    = "${var.region}"
   jar_file                  = "${var.jar_file}"
-  sns_topic                 = "${var.jar_file}"
+  sns_topic                 = ""
   environment_prefix        = "${var.environment_prefix}"
   lambda_role_arn           = "${var.lambda_role_arn}"
 }
